f21c51236d917faf6c8ccf0f7e9ec192aba94c1b
StoatPower <StoatPower@users.noreply.github.com>
2023-04-01 14:09:01 -0400

    added moduledoc for Robot

--- apps/robots_are_fun/lib/robots_are_fun/robot.ex
+++ apps/robots_are_fun/lib/robots_are_fun/robot.ex
@@ -1,3 +1,13 @@
 defmodule RobotsAreFun.Robot do
-  defstruct ~w(battery_level robot_id x y)a
+  @moduledoc """
+  A definition of a `Robot`.
+
+  A robot has the following properties:
+
+  * `:robot_id` - a simple identifier for the robot
+  * `:battery_level` - from 0 to 100 (presumably)
+  * `:x` - the robot's current `x` position
+  * `:y` - the robot's current `y` position
+  """
+  defstruct ~w(robot_id battery_level x y)a
 end
